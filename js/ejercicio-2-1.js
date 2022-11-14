3be28b263ba882836a4a530f95aa1b1e2e6cf042
Sergio Medina <hola@sergiomedina.io>
2022-11-14 20:14:51 -0300

    feat: update message depend on input match

--- js/ejercicio-2-1.js
+++ js/ejercicio-2-1.js
@@ -17,14 +17,20 @@ msj = document.getElementById("mensaje");
 // Asigno una función anónima al evento onclick del botón:
 btn.addEventListener('click', (e) => {
     // Obtengo los values:
-    let pw1 = '';
-    let pw2 = '';
+    let pw1 = cl1.value;
+    let pw2 = cl2.value;
     if (pw1 == '' || pw2.length == 0) {
         // Falta ingresar:
+        msj.innerHTML = "Falta ingresar algún dato";
+        msj.style.color = "blue";
     } else if (pw1 != pw2) {
+        msj.innerHTML = "No coinciden";
+        msj.style.color = "red";
         // No coinciden:
         // Banderita de error:
     } else {
         // Coinciden:
+        msj.innerHTML = "Las contraseñas coinciden.";
+        msj.style.color = "green";
     }
 });
